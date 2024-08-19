7ce56cfa55976c409a13a21c6522bf296b716017
suyan.shrestha <suyan.shrestha@bajratechnologies.com>
2024-08-19 16:44:33 +0545

    feat: add onboarding screen without async storage usage

--- App.jsx
+++ App.jsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import OnboardingScreen from './src/screens/OnboardingScreen/OnboardingScreen';
+import LoginScreen from './src/screens/LoginScreen/LoginScreen';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="OnboadingScreen"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
